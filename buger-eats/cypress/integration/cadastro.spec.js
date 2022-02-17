7f96eac27d670a2648457474b53a0b8901d57591
Diego Martin <diegomartin10@live.com>
2022-02-17 11:37:35 -0300

    Atualização endereço

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -21,7 +21,10 @@ describe('Cadastro', () => {
                 complemento: 'Loja 3',
                 bairro: 'Água Branca',
                 cidade_uf: 'Contagem/MG',
-            }
+            },
+            metodo_entrega: 'Moto',
+            cnh: 'cnh-digital.jpg'
+
         }
 
         cy.get('input[name="name"]').type(entregador.nome)
@@ -34,6 +37,14 @@ describe('Cadastro', () => {
 
         cy.get('input[name="address-number"]').type(entregador.endereco.numero)
         cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
+
+        cy.get('input[name=address]').should('have.value', entregador.endereco.rua)
+        cy.get('input[name=district]').should('have.value', entregador.endereco.bairro)
+        cy.get('input[name=city-uf]').should('have.value', entregador.endereco.cidade_uf)
+
+        cy.contains('.delivery-method li', entregador.metodo_entrega).click()
+
+        cy.get('input[accept^="image"]').attachFile('/images/' + entregador.cnh)
 
     })
 
