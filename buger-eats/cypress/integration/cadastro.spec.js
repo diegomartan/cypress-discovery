b269698b2a012fca22e96426749f88d24c3d64a0
Diego Martin <diegomartin10@live.com>
2022-02-18 10:06:00 -0300

    Validação span de alerta

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -56,5 +56,56 @@ describe('Cadastro', () => {
     })
 
 
+    it('CPF inválido', () => {
+        cy.viewport(1400, 900)
+        cy.visit('https://buger-eats.vercel.app')
+
+        cy.get('a[href="/deliver"]').click()
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+
+        var entregador = {
+            nome: 'Diego Martin',
+            cpf: '000111222AA',
+            email: 'diego@gmail.com',
+            whatsapp: '31999999999',
+            endereco: {
+                cep: '32371000',
+                rua: 'Rua Cardeal Arcoverde',
+                numero: '142',
+                complemento: 'Loja 3',
+                bairro: 'Água Branca',
+                cidade_uf: 'Contagem/MG',
+            },
+            metodo_entrega: 'Moto',
+            cnh: 'cnh-digital.jpg'
+
+        }
+
+        cy.get('input[name="name"]').type(entregador.nome)
+        cy.get('input[name="cpf"]').type(entregador.cpf)
+        cy.get('input[name="email"]').type(entregador.email)
+        cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+        
+        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+        cy.get('input[type="button"][value="Buscar CEP"]').click()
+
+        cy.get('input[name="address-number"]').type(entregador.endereco.numero)
+        cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
+
+        cy.get('input[name=address]').should('have.value', entregador.endereco.rua)
+        cy.get('input[name=district]').should('have.value', entregador.endereco.bairro)
+        cy.get('input[name=city-uf]').should('have.value', entregador.endereco.cidade_uf)
+
+        cy.contains('.delivery-method li', entregador.metodo_entrega).click()
+
+        cy.get('input[accept^="image"]').attachFile('/images/' + entregador.cnh)
+
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.alert-error').should('have.text', 'Oops! CPF inválido')
+    })
+
+
 })
 
