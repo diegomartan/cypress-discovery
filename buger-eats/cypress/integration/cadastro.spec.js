ab370bebf8b258140a4c0fb4c5ffa7da3164b52d
Diego Martin <diegomartin10@live.com>
2022-02-24 13:48:22 -0300

    Introdução de fixtures/json

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -4,48 +4,16 @@ import signup from '../pages/SignupPage'
 
 describe('Cadastro', () => {
 
-    // before(function(){
-    //     cy.log('Tudo aqui é executado uma única vez ANTES de TODOS os testes')
-    // })
+    beforeEach(function () {
+        cy.fixture('deliver').then((d) => {
+            this.deliver = d
+        })
+    })
 
-    // beforeEach(function(){
-    //     cy.log('Tudo aqui é executado sempre ANTES de CADA caso de teste')
-    // })
-
-    // after(function(){
-    //     cy.log('Tudo aqui é executado uma única vez DEPOIS de TODOS os testes')
-    // })
-
-    // afterEach(function(){
-    //     cy.log('Tudo aqui é executado sempre DEPOIS de CADA caso de teste')
-    // })
-
-
-    it('Usuário deve se tornar um entregador', () => {
-
-
-        var deliver = {
-            name: 'Diego Martin',
-            cpf: '00011122234',
-            email: 'diego@gmail.com',
-            whatsapp: '31999999999',
-            address: {
-                postalcode: '32371000',
-                street: 'Rua Cardeal Arcoverde',
-                number: '142',
-                details: 'Loja 3',
-                district: 'Água Branca',
-                city_state: 'Contagem/MG',
-            },
-            deluvery_method: 'Moto',
-            cnh: 'cnh-digital.jpg'
-
-        }
-
-        
+    it('Usuário deve se tornar um entregador', function () {
 
         signup.go()
-        signup.fillForm(deliver)
+        signup.fillForm(this.deliver.signup)
         signup.submit()
 
         const expectedMassage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
@@ -54,32 +22,10 @@ describe('Cadastro', () => {
     })
 
 
-    it('CPF inválido', () => {
-
-        var deliver = {
-            name: 'Diego Martin',
-            cpf: '000111222AA',
-            email: 'diego@gmail.com',
-            whatsapp: '31999999999',
-            address: {
-                postalcode: '32371000',
-                street: 'Rua Cardeal Arcoverde',
-                number: '142',
-                details: 'Loja 3',
-                district: 'Água Branca',
-                city_state: 'Contagem/MG',
-            },
-            deluvery_method: 'Moto',
-            cnh: 'cnh-digital.jpg'
-
-        }
-
-        
-
+    it('CPF inválido', function () {
         signup.go()
-        signup.fillForm(deliver)
+        signup.fillForm(this.deliver.cpf_inv)
         signup.submit()
-
         signup.alertMessageShouldBe('Oops! CPF inválido')
 
     })
